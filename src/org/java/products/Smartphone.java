e0041620955a4c947ab40863622185cf979a0528
Alessandro-Villani <alessvill@libero.it>
2023-05-09 15:12:45 +0200

    added Smartphone class

---
+++ src/org/java/products/Smartphone.java
@@ -0,0 +1,37 @@
+package org.java.products;
+
+public class Smartphone extends Prodotto {
+	
+	private long imei;
+	private String memory;
+
+	public Smartphone(String name, String brand, float price, int vat, long imei, String memory) {
+		
+		super(name, brand, price, vat);
+		setImei(imei);
+		setMemory(memory);
+		
+	}
+
+	public long getImei() {
+		return imei;
+	}
+
+	public void setImei(long imei) {
+		this.imei = imei;
+	}
+
+	public String getMemory() {
+		return memory;
+	}
+
+	public void setMemory(String memory) {
+		this.memory = memory;
+	}
+	
+	@Override
+	public String toString() {
+		return "[SMARTPHONE] " + super.toString() + "Imei: " + getImei() + ", Memoria: " + getMemory();
+	}
+
+}
